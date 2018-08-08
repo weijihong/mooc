ad1f630ec53be1a11d461af5f2f0003efd7ae73e
weijihong <johnnywei1996@gmail.com>
2018-08-08 19:33:09 +0800

    更新readme.md

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # mooc
+
+## [1.Vue+Webpack打造todo应用](https://www.imooc.com/learn/935)
+
+#### 1.1 学习目的
+
+通过webpack+vue2的配置运行一个vue的工程，并使用vue开发一个简单的项目
+
+Webpack: 打包所有资源，脚本的工具，模块化...
+
+Vue2：一款渐进式JavaScript框架，特点：数据绑定，把对dom的操作转化为对数据的操作，组件化开发（数据变化后，会执行render方法更新html内容）
+
+#### 1.2 学习内容
+
+- es6语法
+- vue父子组件通信，计算属性（数据变动才会再次计算，否则使用缓存）
+- Webpack配置前端工程
+- Webpack打包优化
+
+#### 1.3 打包过程
+
+1. 新建文件夹，执行`npm init`，让npm管理这个文件夹（以便后期安装我们所需要的包）
+
+2. `npm i webpack vue vue-loader` 安装webpack，vue，vue-loader(用来加载.vue格式文件)，安装后命令行可能会提醒你安装相关依赖，需根据提示安装后即可
+
+3. 新建src目录放置源码，在其下新建app.vue，每个vue文件就是一个组件
+
+4. 在根目录新建webpack.config.js，设置entry路口为/src/index.js，在index.js里进行挂载到html上（项目启动会识别package.json，所以我们要需要在package.json中添加脚本`"build":  "webpack --config webpack.config.js"`才能执行webpack.config.js）
+
+5. 在webpack.config.js里用正则为.vue类型声明loader：
+
+   ```
+     module: {
+           rules: [
+               {
+                   test: /\.vue$/,
+                   loader: 'vue-loader'
+               },
+   ```
+
+   关于rules还有很多规则需要加载（如.css，图片资源….）
+
+6. 执行`npm run build` 进行打包，在根目录下生成 /dist/bundle.js
+
+#### 1.4 webpack-dev-server（用于开发环境）
+
+- 安装 cross-env（解决不同平台启动dev-server命令不同问题）:	npm i cross-env
+
+- 在package.json中添加脚本
+
+  ```json
+  "build": "cross-env NODE_ENV=production webpack --config webpack.config.js",
+      "dev": "cross-env NODE_ENV=development webpack-dev-server --config webpack.config.js"
+  ```
+
+
+- 判断如果是开发环境，加入一些开发环境配置
+- 需要一个html文件（作为入口）容纳之前打包好的js，需要安装插件：`npm i html-webpack-plugin` 然后在webpack.config.js中进行引入并完成相关配置
+- 执行`npm run dev` 后，在浏览器访问：`127.0.0.1:8000` 访问 
+
+#### 1.5 正式使用VUE2进行项目开发（见业务逻辑代码）
+
+#### 1.6 webpack配置优化
+
+> 由于打包后生成一个js文件，里面包含了css，类库代码，业务代码等复杂代码
+>
+> 但是有些css可能需要单独作为浏览器缓存（减少服务器流量，用户加载速度加快）
+>
+> 以及vue.js源码这类稳定性较高的代码也应该单独打包成一份，在浏览器中进行产长缓存，减少每次重复编译的时间
+
+##### .css文件优化
+
+##### 单独打包类库文件
+
+##### chunkhash和hash的区别
+
+生成环境一定要使用chunkhash因为使用hash生成的串是一样的，当业务代码改变，类库也会重新编译，就没意义了。
+
+
+
+------
+
--- todolist/webpack.config.js
+++ todolist/webpack.config.js
@@ -1,20 +1,27 @@
 const path = require('path')
+// 引入安装后的插件，类似java中的import
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+// 把非js的东西单独打包成静态文件，如有些css要单独做浏览器缓存等
 const ExtractPlugin = require('extract-text-webpack-plugin')
 
+// 判断是否是开发环境
+// 启动脚本时候，设置的环境变量都存在process.env里
 const isDev = process.env.NODE_ENV === 'development'
 const config = {
+    // webpack编译目标是web平台
     target: 'web',
-    // __dirname项目根目录
+    // __dirname项目根目录，指定项目路口文件
     entry: path.join(__dirname, 'src/index.js'),
     output: {
+        // 把index.js里的依赖等设置打包成一个可以直接运行的js文件并输出到下方指定路径
         filename: 'bundle.js',
         path: path.join(__dirname, 'dist')
     },
     module: {
         rules: [
             {
+                // 正则表达式的.需要转义
                 test: /\.vue$/,
                 loader: 'vue-loader'
 
@@ -30,9 +37,13 @@ const config = {
                 test: /\.(gif|jpg|png|jpeg|svg)$/,
                 use: [
                     {
+                        // 把图片转化为base-64代码直接写在js里而不用生成一个新的文件，能够减少Http请求
+                        // url-loader依赖于file-loader
                         loader: 'url-loader',
                         options: {
+                            // 如果资源小于1024，则使用base-64方法处理
                             limit: 1024,
+                            // 指定输出文件名字，[ext]——文件扩展名
                             name: '[name]-aaa.[ext]'
                         }
                     }
@@ -40,8 +51,10 @@ const config = {
             }
         ]
     },
+
     plugins: [
         new webpack.DefinePlugin({
+            // 在此处定义后，在我们自己写的Js代码中可以引用到
             'process.env': {
                 NODE_ENV: isDev ? '"development"' : '"production"'
             }
@@ -50,11 +63,15 @@ const config = {
     ]
 }
 
+// 如果在开发环境下，就加些配置
 if (isDev) {
     config.module.rules.push({
+        // styl文件写样式可以不用在乎括号冒号等，比较简洁
         test: /\.styl/,
         use: [
+            // 把css文件内容加入到html的style标签中
             'style-loader',
+            // 从css文件读出内容
             'css-loader',
             {
                 loader: 'postcss-loader',
@@ -62,58 +79,71 @@ if (isDev) {
                     sourceMap: true,
                 }
             },
+            // 用来读取stylus文件处理后是css文件
             'stylus-loader'
         ]
 
     })
+    // 帮助在页面上调试代码
     config.devtool = '#cheap-module-eval-source-map'
+    // 写webpack-dev-server的配置
+    // 启动后是一个服务，需要监听一个端口
     config.devServer = {
         port: '8000',
         host: '127.0.0.1',
+        // 在webpack编译时如果有错误可以显示在网页上 
         overlay: {
             errors: true,
         },
         // open:true
+        // 改了组件代码只重新渲染组件，而不刷新页面
         hot: true
     }
+    // 需安装热模块代替相关插件
     config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
+        // 减少一些不需要显示的错误信息展示
         new webpack.NoEmitOnErrorsPlugin()
     )
-}else{
+} else {
     config.entry = {
-        app : path.join(__dirname, 'src/index.js'),
-        vendor:['vue']
+        app: path.join(__dirname, 'src/index.js'),
+        vendor: ['vue']
     }
+    // 生产环境文件命名要用chunkhash,开发环境不能用
     config.output.filename = '[name].[chunkhash:8].js'
     config.module.rules.push(
         {
             test: /\.styl/,
-            use:ExtractPlugin.extract({
+            use: ExtractPlugin.extract({
                 fallback: 'style-loader',
                 use: [
                     'css-loader',
                     {
                         loader: 'postcss-loader',
                         options: {
+                            // stylus-loader编译后会生成sourceMap,因此postcss-loader可以直接使用，提升编译效率
                             sourceMap: true,
                         }
                     },
                     'stylus-loader'
                 ]
-             
+
 
             })
-            
+
         }
     )
     config.plugins.push(
+        // 指定单独输出的css的名字（默认Vue中的css不会打包））
         new ExtractPlugin('styles.[contentHash:8].css'),
+        // 指定单独输出js
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor'
         }),
+        // 在app.js中一些和webpack相关的代码单独打包（当有新的包插入后，webpack会给模块加入id,如果插入的包在中间，后面的id就会变化，就会导致打包出来的hash值变化，就会失去浏览器长缓存的效果）
         new webpack.optimize.CommonsChunkPlugin({
-            name :'runtime'
+            name: 'runtime'
         })
     )
 }
